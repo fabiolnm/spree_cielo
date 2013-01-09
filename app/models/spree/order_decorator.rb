75613466e4c62f39d7fe080dc07a1b5dc5f64439
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2013-01-09 07:25:02 -0200

    Rejects non-selected payment methods

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,8 @@
 Spree::Order.class_eval do
+  accepts_nested_attributes_for :payments, reject_if: proc { |attrs|
+    attrs[:payment_method_id].blank?
+  }
+
   def available_payment_options
     @available_payment_options ||= available_payment_methods.collect { |m|
       payments.build payment_method_id: m.id, source_attributes: { }
