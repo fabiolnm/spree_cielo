c835fd39488f92080f73eaef603443ea652921a0
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2013-01-11 22:52:06 -0200

    Configures api_key, api_number and soft_descriptor as Gateway preferences

--- app/models/spree_cielo/hosted_buy_page_payment.rb
+++ app/models/spree_cielo/hosted_buy_page_payment.rb
@@ -5,6 +5,19 @@ module SpreeCielo
     has_one :payment, class_name: 'Spree::Payment', foreign_key: :source_id
 
     class Gateway < Spree::Gateway
+      attr_accessible :preferred_api_number,
+        :preferred_api_key, :preferred_soft_descriptor
+
+      preference :api_number, :string
+      preference :api_key, :string
+      preference :soft_descriptor, :string
+
+      after_initialize do
+        self.preferred_api_number      = api_number
+        self.preferred_api_key         = api_key
+        self.preferred_soft_descriptor = soft_descriptor
+      end
+
       def payment_source_class
         HostedBuyPagePayment
       end
@@ -16,6 +29,26 @@ module SpreeCielo
       # determine what partial will be rendered for this method type
       def method_type
         "cielo_hosted"
+      end
+
+      def api_number
+        if preferences[:test_mode]
+          Cieloz::DadosEc::TEST_MOD_CIELO.numero
+        else
+          preferences[:api_number]
+        end
+      end
+
+      def api_key
+        if preferences[:test_mode]
+          Cieloz::DadosEc::TEST_MOD_CIELO.chave
+        else
+          preferences[:api_key]
+        end
+      end
+
+      def soft_descriptor
+        preferences[:soft_descriptor] || ""
       end
 
       def authorize(money, source, options)
