b3be07a2bff892f8dd008a5d944109e94d2a9224
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2013-01-16 09:19:11 -0200

    Removes _payment partial replacement - 'corrected' nested form view did't worked together with other payment methods, being necessary to use the poor 'manual' payment_source inputs

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,33 +6,20 @@ Spree::CheckoutController.class_eval do
   end
 
   private
-  def object_params
-    # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
-    if @order.payment?
-      if params[:payment_source].present? && source_params = params.delete(:payment_source)[params[:order][:payments_attributes].first[:payment_method_id].underscore]
-        params[:order][:payments_attributes].first[:source_attributes] = source_params
-      end
-      # this code is brittle and was moved to a before_save callback
-      #if (params[:order][:payments_attributes])
-      #  params[:order][:payments_attributes].first[:amount] = @order.total
-      #end
-    end
-    params[:order]
-  end
-
   def request_authorization_at_cielo_hosted_page
     return unless request.put? and params[:state] == "payment"
 
-    payment = Spree::Order.new(params[:order]).payment
+    load_order
+    payment = Spree::Order.new(object_params).payment
     if payment.valid?
       method = payment.payment_method
       if method.type == "SpreeCielo::HostedBuyPagePayment::Gateway"
-        load_order
         source = payment.source
 
         callback_url = request.url.gsub request.path, cielo_callback_path
         txn = method.authorization_transaction @order, source, callback_url
 
+        # FIXME handle transaction when response is error
         payment = @order.payments.create source_attributes: {
           xml: txn.xml,
           tid: txn.tid,
--- app/models/spree/payment_decorator.rb
+++
@@ -1,7 +0,0 @@
-Spree::Payment.class_eval do
-  after_save :set_order_amount
-
-  def set_order_amount
-    update_column :amount, order.amount
-  end
-end
